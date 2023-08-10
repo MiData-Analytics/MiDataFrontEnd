c0e19a3df58c0c822a47e5a8ed4f6fee95310555
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-10 17:54:15 +0100

    update: mobile responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-[#DFD4FF] h-fit py-[3.5rem] px-10 relative bottom-0 w-full">
+    <footer className="bg-[#DFD4FF] h-fit py-[3.5rem] sm:px-10 px-7 relative bottom-0 w-full">
       <div className="flex justify-between items-center py-[3rem] relative">
         {/* <Image
           src="/bg/ball.png"
@@ -22,7 +22,7 @@ export default function Footer() {
         <div>
           <Image src="/logo/midata.png" width={160} height={39} className="" />
         </div>
-        <div className="flex">
+        <div className="hidden flex-row sm:flex">
           <Link href="/">
             <Image src="/icons/twitter.png" width={45} height={45} />
           </Link>
@@ -44,71 +44,96 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex justify-center">
-        <hr className="w-[95%] border-[#37373771]" />
+        <hr className="sm:w-[95%] w-[100%] border-[#37373771]" />
       </div>
-      <div className="flex w-[90%] justify-between mx-auto my-5 items-center sm:flex-row flex-col">
-        <h3 className="text-[#37139D] font-extrabold text-5xl leading-[3rem]">
+      <div className="flex flex-row sm:hidden">
+        <Link href="/">
+          <Image src="/icons/twitter.png" width={45} height={45} />
+        </Link>
+        <Link href="/">
+          <Image src="/icons/youtube.png" width={45} height={45} />
+        </Link>
+        <Link href="/">
+          <Image src="/icons/facebook.png" width={45} height={45} />
+        </Link>
+        <Link href="/">
+          <Image src="/icons/linkedin.png" width={45} height={45} />
+        </Link>
+        <Link href="/">
+          <Image src="/icons/instagram.png" width={45} height={45} />
+        </Link>
+        <Link href="/">
+          <Image src="/icons/thread.png" width={45} height={45} />
+        </Link>
+      </div>
+      <div className="flex w-[90%] justify-between mx-auto my-5 items-center sm:flex-row flex-col-reverse">
+        <h3 className="text-[#37139D] font-extrabold sm:text-5xl text-4xl leading-[3rem]">
           See the big
           <br /> picture in
           <br /> every detail
         </h3>
-        <div className="space-y-2">
-          <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
-          <ul className="space-y-2 font-medium">
-            <li>
-              <Link href="/">Why MiData?</Link>
-            </li>
-            <li>
-              <Link href="/">Security</Link>
-            </li>
-            <li>
-              <Link href="/">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link href="/">User Agreement</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="space-y-2">
-          <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
-          <ul className="space-y-2 font-medium">
-            <li>
-              <Link href="/">Why MiData?</Link>
-            </li>
-            <li>
-              <Link href="/">Security</Link>
-            </li>
-            <li>
-              <Link href="/">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link href="/">User Agreement</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="space-y-2">
-          <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
-          <ul className="space-y-2 font-medium">
-            <li>
-              <Link href="/">Why MiData?</Link>
-            </li>
-            <li>
-              <Link href="/">Security</Link>
-            </li>
-            <li>
-              <Link href="/">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link href="/">User Agreement</Link>
-            </li>
-          </ul>
+        <div className="flex items-center gap-x-3">
+          <div className="space-y-2 py-3">
+            <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
+            <ul className="space-y-2 font-medium">
+              <li>
+                <Link href="/">Why MiData?</Link>
+              </li>
+              <li>
+                <Link href="/">Security</Link>
+              </li>
+              <li>
+                <Link href="/">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link href="/">User Agreement</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-2">
+            <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
+            <ul className="space-y-2 font-medium">
+              <li>
+                <Link href="/">Why MiData?</Link>
+              </li>
+              <li>
+                <Link href="/">Security</Link>
+              </li>
+              <li>
+                <Link href="/">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link href="/">User Agreement</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-2">
+            <h5 className="font-bold text-[#583E9C]">ABOUT MiData</h5>
+            <ul className="space-y-2 font-medium">
+              <li>
+                <Link href="/">Why MiData?</Link>
+              </li>
+              <li>
+                <Link href="/">Security</Link>
+              </li>
+              <li>
+                <Link href="/">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link href="/">User Agreement</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="flex justify-center">
         <hr className="w-[95%] border-[#37373771]" />
       </div>
-      <div className="w-[95%] mx-auto">
-        <p className="text-right text-sm font-medium pt-5">
+      <div className="w-[95%] mx-auto pt-5 text-[#373737]">
+        <p className="text-center text-[10px]">
+          Powered By: Kimpact Development Initiative
+        </p>
+        <p className="sm:text-right text-center text-sm font-medium">
           Copyright &copy; {currentYear} MiData - All Rights Reserved
         </p>
       </div>
