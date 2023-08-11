cb18883c8577a6729f2222638449bb659a5ed245
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-11 16:53:41 +0100

    update: set up shapes component

--- src/components/Shape/index.js
+++ src/components/Shape/index.js
@@ -4,14 +4,14 @@ export function Tag({ iconURI, heading, text, color, direction }) {
   return (
     <div className="relative">
       <div
-        className={`absolute top-5 z-10 flex items-start ${
+        className={`absolute top-5 z-10 flex items-start w-[80%] ${
           direction === "left" ? "flex-row left-2" : "flex-row-reverse right-0"
         }`}
       >
         <Image
           src={iconURI ? iconURI : "/icons/db.png"}
-          width={125}
-          height={125}
+          width={145}
+          height={145}
           alt={heading}
           className="relative object-contain"
         />
@@ -19,17 +19,9 @@ export function Tag({ iconURI, heading, text, color, direction }) {
           <h3 className="text-3xl font-bold">
             {heading ? heading : "Data Collection"}
           </h3>
-          {text ? (
-            text
-          ) : (
-            <p>
-              The tool aims at simplifying data reporting
-              <br /> with analytics and visualization. It will enable <br />{" "}
-              researchers to have control over the data <br />
-              reporting process for sensitive data collection
-              <br /> without limiting participation.
-            </p>
-          )}
+          <p className="font-thin text-md">
+            {text}
+          </p>
         </div>
       </div>
       {direction === "left" ? (
@@ -61,4 +53,114 @@ export function Tag({ iconURI, heading, text, color, direction }) {
       )}
     </div>
   );
+}
+
+export function Clip({ iconURI, heading, text, color, clip }) {
+  if (clip === "top") {
+    return (
+      <div className="relative">
+        <div
+          className={`absolute top-8 z-10 flex items-center justify-center flex-col text-center`}
+        >
+          <Image
+            src={iconURI ? iconURI : "/icons/db.png"}
+            width={45}
+            height={45}
+            alt={heading}
+            className="relative object-contain"
+          />
+          <div className="flex flex-col gap-y-3 justify-center items-center">
+            <h3 className="text-xl font-bold">
+              {heading ? heading : "Data Collection"}
+            </h3>
+            <p className="font-thin text-[0.68rem]  w-[95%]">{text}</p>
+          </div>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="298"
+          height="226"
+          viewBox="0 0 298 186"
+          fill="none"
+        >
+          <path
+            d="M298 161C298 174.807 286.807 186 273 186L25.0001 186C11.1929 186 2.95119e-05 174.807 2.82713e-05 161L2.30748e-05 103.166L1.73245e-05 39.1679C1.61809e-05 26.4402 9.56233 15.7442 22.2106 14.324L146.982 0.314847C148.845 0.105604 150.726 0.106717 152.59 0.318143L275.819 14.3009C288.454 15.7346 298 26.425 298 39.1415L298 161Z"
+            fill={color ? color : "#DFD4FF"}
+          />
+        </svg>
+      </div>
+    );
+  }
+
+  if (clip === "middle") {
+    return (
+      <div className="relative h-fit">
+        <div
+          className={`absolute top-2 z-10 flex items-center justify-center flex-col text-center`}
+        >
+          <Image
+            src={iconURI ? iconURI : "/icons/db.png"}
+            width={45}
+            height={45}
+            alt={heading}
+            className="relative object-contain"
+          />
+          <div className="flex flex-col gap-y-3 justify-center items-center">
+            <h3 className="text-xl font-bold">
+              {heading ? heading : "Data Collection"}
+            </h3>
+            <p className="font-thin text-[0.68rem] w-[95%]">{text}</p>
+          </div>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="298"
+          height="169"
+          viewBox="0 0 298 169"
+          fill="none"
+        >
+          <path
+            d="M-4.25135e-06 25C-1.90339e-06 11.1929 11.1929 1.95703e-06 25 4.37114e-06L273 4.77328e-05C286.807 5.0147e-05 298 11.1929 298 25.0001L298 82.7448L298 144C298 157.807 286.807 169 273 169L145.959 169L25 169C11.1928 169 -2.68357e-05 157.807 -2.44878e-05 144L-4.25135e-06 25Z"
+            fill={color}
+          />
+        </svg>
+      </div>
+    );
+  }
+
+  if (clip === "bottom") {
+    return (
+      <div className="relative">
+        <div
+          className={`absolute top-0 z-10 flex items-center justify-center flex-col text-center`}
+        >
+          <Image
+            src={iconURI ? iconURI : "/icons/db.png"}
+            width={45}
+            height={45}
+            alt={heading}
+            className="relative object-contain"
+          />
+          <div className="flex flex-col gap-y-3 justify-center items-center">
+            <h3 className="text-xl font-bold">
+              {heading ? heading : "Data Collection"}
+            </h3>
+            <p className="font-thin text-[0.68rem] w-[95%]">{text}</p>
+          </div>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="298"
+          height="186"
+          viewBox="0 0 298 186"
+          fill="none"
+        >
+          <path
+            d="M-4.25135e-06 25C-1.9034e-06 11.1929 11.1929 1.95703e-06 25 4.37114e-06L273 4.77328e-05C286.807 5.0147e-05 298 11.1929 298 25.0001L298 82.834L298 146.832C298 159.56 288.438 170.256 275.789 171.676L151.018 185.685C149.155 185.894 147.274 185.893 145.41 185.682L22.1813 171.699C9.54588 170.265 -2.71364e-05 159.575 -2.49739e-05 146.858L-4.25135e-06 25Z"
+            fill={color}
+          />
+        </svg>
+      </div>
+    );
+  }
 }
