f6781d19ef3f78a480c4b423ec253e9672a238ea
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-11 18:55:20 +0100

    update: added components to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
-import Head from 'next/head'
-import Navbar from '@/components/Navbar'
+import Head from "next/head";
+import Layout from "@/layouts/Main";
+import { Collaborate } from "@/components/Collaborate";
+import { DataReport } from "@/components/DataReport";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <Layout>
       <Head>
         <title>MiData | Landing Page</title>
       </Head>
-      <Navbar />
-    </main>
-  )
+      <Hero />
+      <DataReport />
+      <Collaborate />
+    </Layout>
+  );
 }
