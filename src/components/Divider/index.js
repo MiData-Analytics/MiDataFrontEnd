e5a9776cd7e7c8948d5e455f7643854349fb8143
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-12 20:47:35 +0100

    update: added props

--- src/components/Divider/index.js
+++ src/components/Divider/index.js
@@ -1,8 +1,8 @@
-export function Divider(){
+export function Divider({color}){
     return (
       <div className="flex items-center mt-4">
         <div className="flex-grow h-px bg-gray-300"></div>
-        <div className="mx-4 text-white">OR</div>
+        <div className={`mx-4 ${color ? color : 'text-black'}`}>OR</div>
         <div className="flex-grow h-px bg-gray-300"></div>
       </div>
     );
