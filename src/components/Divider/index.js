3f2ebb31fcee0ad7f444e05db429f8b954152a56
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-12 20:22:07 +0100

    add: created divider component

---
+++ src/components/Divider/index.js
@@ -0,0 +1,9 @@
+export function Divider(){
+    return (
+      <div className="flex items-center mt-4">
+        <div className="flex-grow h-px bg-gray-300"></div>
+        <div className="mx-4 text-white">OR</div>
+        <div className="flex-grow h-px bg-gray-300"></div>
+      </div>
+    );
+}
