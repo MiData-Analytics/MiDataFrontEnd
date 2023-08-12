48402ac2bc35b698de21fb77fecf7453c6edce17
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-12 20:39:07 +0100

    add: end point list

---
+++ src/utils/urls.js
@@ -0,0 +1,8 @@
+export const baseUrl = 'http://localhost:1337'
+
+export const urls = {
+  socket: `${baseUrl}`,
+  login: `${baseUrl}/api/v1/user/login`,
+  signup: `$${baseUrl}/api/v1/user/signup`,
+
+};
