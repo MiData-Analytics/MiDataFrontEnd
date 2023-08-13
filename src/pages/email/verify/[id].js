37fa9574f1937500ee7708ea4e56a2772a885899
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-13 19:55:35 +0100

    add: started work on email verification page

---
+++ src/pages/email/verify/[id].js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import axios from "axios";
+import Layout from "@/layouts/Main";
+import { useRouter } from "next/router";
+
+export default function EmailVerifyPage() {
+  const { query } = useRouter();
+
+  
+
+  return (
+    <Layout>
+      <Head>
+        <title>MiData | Email Verification</title>
+      </Head>
+    </Layout>
+  );
+}
