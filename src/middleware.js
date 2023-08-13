9719bb12ef2004276680cde7a598fc8b9db950da
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-13 18:09:50 +0100

    add: set up middleware for authentication

---
+++ src/middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://midatatest.netlify.app/"
+    : "http://localhost:1337";
+
+export async function middleware(req,res){
+    const url = req.url
+    const cookies = req.cookies
+    const token = cookies.get("token");
+
+    if(url.includes('dashboard')){
+        if(!token){
+            return NextResponse.redirect(`${baseUrl}/login`);
+        }
+    }
+
+    if(url.includes('login') || url.includes('signup')){
+        if(token){
+            return NextResponse.redirect(`${baseUrl}/dashboard`)
+        }
+    }
+
+    return NextResponse.next()
+}
