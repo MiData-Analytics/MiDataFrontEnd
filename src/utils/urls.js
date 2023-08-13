7ed1e043a56c3ee3bb1818aab3b7a1688c798e05
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-13 18:07:21 +0100

    update: set environment

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,10 @@
-export const baseUrl = 'http://localhost:1337'
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://midata.cyclic.app"
+    : "http://localhost:1337";
 
 export const urls = {
   socket: `${baseUrl}`,
   login: `${baseUrl}/api/v1/user/login`,
   signup: `$${baseUrl}/api/v1/user/signup`,
-
 };
