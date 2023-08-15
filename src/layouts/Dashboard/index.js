f3ef9c3d04bff357336ec3ece2a960194788f0c8
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-15 22:10:14 +0100

    update: set cursor behavior

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -2,7 +2,7 @@ import { Sidebar, Header } from "@/components/Dashboard";
 
 export function DashboardLayout({children}){
     return (
-      <section className="w-full flex lg:bg-[#f5f5f5] bg-[#fff] lg:flex-row flex-col">
+      <section className="w-full flex lg:bg-[#f5f5f5] bg-[#fff] lg:flex-row flex-col cursor-default">
         <Sidebar />
         <div className="flex w-full flex-col">
           <Header />
