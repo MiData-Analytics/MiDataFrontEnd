5d2a9ea2cca4be4f999d65d484ce361aaa99c5a6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-16 11:50:05 +0100

    update: fixed typo

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -1,9 +1,9 @@
 import { Sidebar, Header } from "@/components/Dashboard";
-import { UserContext } from "@/contexts/UserContext";
+import UserProvider from "@/contexts/UserContext";
 
 export function DashboardLayout({children}){
     return (
-      <UserContext>
+      <UserProvider>
         <section className="w-full flex lg:bg-[#f5f5f5] bg-[#fff] lg:flex-row flex-col cursor-default">
           <Sidebar />
           <div className="flex w-full flex-col">
@@ -11,6 +11,6 @@ export function DashboardLayout({children}){
             <div className="w-full h-fit p-3">{children}</div>
           </div>
         </section>
-      </UserContext>
+      </UserProvider>
     );
 }
