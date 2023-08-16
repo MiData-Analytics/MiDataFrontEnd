b3600c958681fd6bfa182b4d3c4a5ab65588968d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-16 20:21:05 +0100

    update: removed provider

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -1,16 +1,13 @@
 import { Sidebar, Header } from "@/components/Dashboard";
-import UserProvider from "@/contexts/UserContext";
 
-export function DashboardLayout({children}){
-    return (
-      <UserProvider>
-        <section className="w-full flex lg:bg-[#f5f5f5] bg-[#fff] lg:flex-row flex-col cursor-default">
-          <Sidebar />
-          <div className="flex w-full flex-col">
-            <Header />
-            <div className="w-full h-fit p-3">{children}</div>
-          </div>
-        </section>
-      </UserProvider>
-    );
+export function DashboardLayout({ children }) {
+  return (
+    <section className="w-full flex lg:bg-[#f5f5f5] bg-[#fff] lg:flex-row flex-col cursor-default">
+      <Sidebar />
+      <div className="flex w-full flex-col">
+        <Header />
+        <div className="w-full h-fit p-3">{children}</div>
+      </div>
+    </section>
+  );
 }
