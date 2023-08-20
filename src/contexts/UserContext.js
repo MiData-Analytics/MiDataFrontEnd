16255adfee65b77455677afa882e4e39bab1c753
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-20 04:46:10 +0100

    update: updated user context

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { urls } from "@/utils/urls";
@@ -11,11 +11,12 @@ export const UserContext = createContext({
 });
 
 const UserProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setError] = useState(null);
   const [cookies] = useCookies(["token"]);
   const [token, setToken] = useState(cookies.token);
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(null); // Store last updatedAt timestamp
 
   const router = useRouter();
   const { route } = router;
@@ -31,8 +32,21 @@ const UserProvider = ({ children }) => {
             },
           });
 
-          setUserData(response.data);
-          setIsLoading(false);
+          // Check if updatedAt has changed before updating the userData and cache
+          if (response.data.updatedAt !== lastUpdatedAt) {
+            setUserData(response.data);
+            setIsLoading(false);
+            setLastUpdatedAt(response.data.updatedAt);
+
+            // Cache the user data and lastUpdatedAt in local storage
+            localStorage.setItem("userData", JSON.stringify(response.data));
+            localStorage.setItem("lastUpdatedAt", response.data.updatedAt);
+          } else {
+            // If updatedAt hasn't changed, use cached data
+            const cachedUserData = localStorage.getItem("userData");
+            setUserData(JSON.parse(cachedUserData));
+            setIsLoading(false);
+          }
         } catch (error) {
           console.error("Error fetching user data", error);
           setError(true);
@@ -41,8 +55,17 @@ const UserProvider = ({ children }) => {
       }
     };
 
-    fetchData();
-  }, []);
+    // Check if lastUpdatedAt is cached in local storage
+    const cachedLastUpdatedAt = localStorage.getItem("lastUpdatedAt");
+    if (cachedLastUpdatedAt) {
+      setLastUpdatedAt(cachedLastUpdatedAt);
+    }
+
+    // Fetch data if lastUpdatedAt is not available or has changed
+    if (!lastUpdatedAt || (route.includes("dashboard") && token)) {
+      fetchData();
+    }
+  }, []); // Re-fetch when lastUpdatedAt changes
 
   return (
     <UserContext.Provider
