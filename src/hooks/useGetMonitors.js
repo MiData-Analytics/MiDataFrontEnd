fe725a56c030d3a7c60de8ceff817ddf5e70f0e7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-04 11:57:08 +0100

    update: set up get monitor profile hook

--- src/hooks/useGetMonitors.js
+++ src/hooks/useGetMonitors.js
@@ -36,3 +36,39 @@ export function useGetMonitors() {
     isError,
   };
 }
+
+export function useGetMonitor() {
+  const [monitor, setMonitor] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const { token } = useCookie();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(urls.getMonitorProfile, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        console.log(response.data)
+
+        setMonitor(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data", error);
+        setError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    monitor,
+    isLoading,
+    isError,
+  };
+}
