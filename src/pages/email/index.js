2d7aa7d120bc22c920804d7d1f052a9cde34e44d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-04 07:31:53 +0100

    add: work on successfully created account page

---
+++ src/pages/email/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "@/layouts/Main";
+
+export default function Email(){
+    return (
+        <Layout>
+            <Head>
+                <title>MiData | Successfully created Account</title>
+            </Head>
+
+        </Layout>
+    )
+}
