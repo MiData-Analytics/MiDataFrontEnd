4f4d8d1eefa18451d2ae48b904a7eb884f54efbe
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-17 12:06:51 +0100

    update: set alignment

--- src/layouts/Monitor/index.js
+++ src/layouts/Monitor/index.js
@@ -2,10 +2,10 @@ import Navbar from "@/components/Navbar";
 
 export default function Layout({ children, userData }) {
   return (
-    <div className="w-full h-full cursor-default flex flex-col items-center justify-between">
+    <div className="w-full h-full cursor-default flex flex-col items-center justify-between relative">
       <Navbar userData={userData} />
       {children}
-      <div className="bg-[#583E9C] w-full">
+      <div className="bg-[#583E9C] w-full absolute bottom-0 mt-3">
         <p className="text-center text-white">
           Copyright © {new Date().getFullYear()} MiData - All Rights Reserved
         </p>
