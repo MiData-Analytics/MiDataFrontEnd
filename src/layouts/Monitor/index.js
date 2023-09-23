316cc48d4a3b00b750c096089c80dbcd2ffe2d92
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-23 18:12:46 +0100

    update: modify spacing

--- src/layouts/Monitor/index.js
+++ src/layouts/Monitor/index.js
@@ -2,10 +2,10 @@ import Navbar from "@/components/Navbar";
 
 export default function Layout({ children, userData }) {
   return (
-    <div className="w-full h-full cursor-default flex flex-col items-center justify-between relative">
+    <div className="w-full h-[100vh] cursor-default flex flex-col items-center justify-between relative">
       <Navbar userData={userData} />
       {children}
-      <div className="bg-[#583E9C] w-full absolute bottom-0 mt-3">
+      <div className="bg-[#583E9C] w-full absolute bottom-0 pt-10">
         <p className="text-center text-white">
           Copyright © {new Date().getFullYear()} MiData - All Rights Reserved
         </p>
